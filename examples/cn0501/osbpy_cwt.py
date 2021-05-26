87363807919ff17d5596ae670d953f5ee7816472
jsantos10 <jesse.santos@analog.com>
2021-05-26 16:53:04 +0800

    Fix Spectrogram Plot
    Fix time scale and display

--- examples/cn0501/osbpy_cwt.py
+++ examples/cn0501/osbpy_cwt.py
@@ -22,11 +22,12 @@ m2k_data = np.asarray(pd.read_csv(fpath+m2k_diff))
 m2k_data = np.reshape(m2k_data, [np.size(m2k_data)])
 
 npts = np.size(m2k_data)
-dt = .05
+dt = 2/npts
 
 t = np.linspace(0, dt * npts, npts)
 f_min = 1
 f_max = 1000
+
 '''
 '''
 
@@ -34,19 +35,24 @@ print("CWT")
 scalogram = cwt(m2k_data, dt, 8, f_min, f_max)
 
 print("Start plot")
-fig = plt.figure()
-ax = fig.add_subplot(111)
+fig = plt.figure(1)
+ax = fig.add_subplot(211)
 
 x, y = np.meshgrid(
     t,
     np.logspace(np.log10(f_min), np.log10(f_max), scalogram.shape[0]))
 
 ax.pcolormesh(x, y, np.abs(scalogram), cmap=obspy_sequential)
-ax.set_xlabel("Time after %s [s]" % 0)
+ax.set_xlabel("Time [s]")
 ax.set_ylabel("Frequency [Hz]")
 ax.set_yscale('log')
 ax.set_ylim(f_min, f_max)
 '''
 '''
+m2k_o = fig.add_subplot(212)
+m2k_o.plot(t,m2k_data)
+m2k_o.set_xlabel("Time [s]")
+m2k_o.set_ylabel("Amplitude [V]")
+m2k_o.set_ylim(0, 5)
 
 plt.show()
